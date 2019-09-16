4c5d3c45e092e0f23895f4cd9df6a5b43547ca0c
James Wonsever <james.wonsever@alphaflow.com>
2019-09-15 19:46:05 -0700

    Better interaction with the web UI

--- app.js
+++ app.js
@@ -2,7 +2,5 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-
-    app.use(express.static('.'))
-
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.static('.'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- parser.js
+++ parser.js
@@ -1,18 +1,67 @@
 // Requiring fs module in which writeFile function is defined.
 const fs = require("fs");
 const m = require("./measure.js");
+const { exec } = require("child_process");
+var open = require("open");
 
-var s = fs
-  .readFileSync("data.json")
+if (process.argv.length == 2) {
+  console.log(
+    "Must supply an input file. ex: `node parser.js input.json [flags]`"
+  );
+  process.exit(1);
+}
+
+// Read input paramters.
+let keyMode = false;
+let arrayMode = false;
+for (let j = 3; j < process.argv.length; j++) {
+  if (process.argv[j] == "-a") {
+    arrayMode = true;
+  } else if (process.argv[j] == "-k") {
+    keyMode = true;
+  } else {
+    console.log("Unknown arg, skipping: -> " + process.argv[j]);
+  }
+}
+
+let s = fs
+  .readFileSync(process.argv[2])
   .toString()
   .trim();
 
+// Make them mode arg for measure
+let mode = "";
+if (keyMode) {
+  mode = mode + "k";
+}
+if (arrayMode) {
+  mode = mode + "a";
+}
+
 // Measure the input file.
-var output = m.measureObject(s, "root", "ak");
+let output = m.measureObject(s, "root", mode);
 console.log(output);
 
-// Write data in 'flame.json'.
-fs.writeFile("flame.json", JSON.stringify(output), err => {
+// Write data in 'out.json'.
+fs.writeFile("out.json", JSON.stringify(output), err => {
   // In case of a error throw err.
   if (err) throw err;
 });
+
+// Spawn a node process to open the app.
+const child = exec(`node app.js`);
+child.stdout.setEncoding("utf8");
+child.stdout.on("data", chunk => {
+  console.log(chunk);
+});
+
+child.stderr.on("data", chunk => {
+  console.log(chunk);
+});
+
+child.on("close", code => {
+  console.log(`child process exited with code ${code}`);
+});
+
+// opens the url in the default browser
+open("http://localhost:5000/flame.html");
